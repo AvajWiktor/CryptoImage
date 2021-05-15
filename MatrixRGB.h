afd6e03fe8d624bc13f29eb5d6d4ecfa3b31fa7a
Wiktor Markowski <wiktor.markowski@student.put.poznan.pl>
2021-05-15 21:23:03 +0100

    Code refactoring

--- MatrixRGB.h
+++ MatrixRGB.h
@@ -13,31 +13,25 @@ using namespace cv;
 class MatrixRGB
 {
 public:
-
 	MatrixRGB(string originalone) 
     {
         _OriginalImagePath = originalone;
         makeMatrix();
 	}
-
-
     string decodeMessage(string imagePath) 
     {
-
         _NewImagePath = imagePath;
         Mat NewImage = imread(_NewImagePath, IMREAD_COLOR); 
 
         if (NewImage.empty())
         {
             return "Could not read the image: "+ _NewImagePath;
-            
         }
 
         for (int i = 0; i < _y; i++)
         {
             for (int j = 0; j < _x; j++)
             {
-
                 Vec3b buf = NewImage.at<Vec3b>(i, j);
                 _NewMatrix[i][j] = buf;
             }
@@ -50,10 +44,8 @@ public:
         {
             for (int j = 0; j < _x; j++)
             {
-
                 if (_OriginalMatrix[i][j].val[0] != _NewMatrix[i][j].val[0])
                 {
-                    
                     s += to_string(abs(_OriginalMatrix[i][j].val[0] - _NewMatrix[i][j].val[0]) - 1);
                     k++;
                 }
@@ -73,11 +65,8 @@ public:
                     _DecodedMessage += stoi(s, 0, 2);
                     s.clear();
                 }
-
             }
-
         }
-
         return _DecodedMessage;
     }
     void encodeMessage(string msg, string messagePath) 
@@ -91,6 +80,7 @@ public:
         uint16_t messageSize = msg.length();
         uint16_t k = 0;
         string s;
+
         for (int i = 0; i < messageSize; i++)
         {
              s += bitset<8>(msg[i]).to_string();
@@ -98,26 +88,16 @@ public:
 
         for (int i = randomYPosition; (i < img.rows)&&(k<s.length()); i++)
         {
-            //uint16_t randomXPosition = rand() % _x + 0;
-
             for (int j = randomXPosition; (j < img.cols) && (k < s.length()); j++)
             {
-               
-                //uint8_t x = rand() % 3 + 0;
-                
                 string temp;
                 temp += s[k];
                 uint8_t x = (1+stoi(temp));
                 uint8_t v = _MessageMatrix[i][j][0];
                 v -= x;
-               // v[1] -= x;
-               // v[2] -= x;
                 _MessageMatrix[i][j].val[0] = v;
-               // _MessageMatrix[i][j].val[1] = v.val[1];
-               // _MessageMatrix[i][j].val[2] = v.val[2];
                 k++;
             } 
-
         }
 
         for (int i = 0; i < img.rows; i++)
@@ -127,29 +107,12 @@ public:
                 img.at<Vec3b>(i, j).val[0] = _MessageMatrix[i][j].val[0];
                 img.at<Vec3b>(i, j).val[1] = _MessageMatrix[i][j].val[1];
                 img.at<Vec3b>(i, j).val[2] = _MessageMatrix[i][j].val[2];
-                  
             }
         }
-       
-    //    showMatrixes();
+
         imwrite(_MessageImagePath,img);
-
-       /* Mat test = imread(_MessageImagePath, IMREAD_UNCHANGED);
-        for (int i = 0; i < _y; i++)
-        {
-            for (int j = 0; j < _x; j++)
-            {
-                cout << "XIm: " << j << ", YIm: " << i << ",B:" << int(test.at<Vec3b>(i,j).val[0]) << ",G:" << int(test.at<Vec3b>(i, j).val[1]) << ",R:" << int(test.at<Vec3b>(i, j).val[2]) << endl;
-            }
-        }
-        */
     }
-
-
-
-
     void showMatrixes() {
-
         for (int i = 0; i < _y; i++)
         {
             for (int j = 0; j < _x; j++)
@@ -158,25 +121,21 @@ public:
               //  cout << "X2: " << j << ", Y2: " << i <<",R:"<< int(_MessageMatrix[i][j][0]) << ",G:" << int(_MessageMatrix[i][j][1]) << ",B:" << int(_MessageMatrix[i][j][2]) << endl;
               //  cout << "X3: " << j << ", Y3: " << i << ",B:" << int(_NewMatrix[i][j][0]) << ",G:" << int(_NewMatrix[i][j][1]) << ",R:" << int(_NewMatrix[i][j][2]) << endl;
             }
-
         }
     }
 private:
     //functions
 	void makeMatrix() 
     {
-        
         Mat OriginalImage = imread(_OriginalImagePath, IMREAD_COLOR);
-        
         Vec3b buf;
         _y = OriginalImage.rows;
         _x = OriginalImage.cols;
+
         if (OriginalImage.empty())
         {
             std::cout << "Could not read the image: " << _OriginalImagePath << std::endl;
         }
-       
-        
         //creating image size matrix filled by zeros
         for (int i = 0; i < _y; i++) 
         {
@@ -185,41 +144,22 @@ private:
             {
                 Vec3b a = Vec3b(0,0,0);
                 rowBuf.push_back(a);
-
             }
             _OriginalMatrix.push_back(rowBuf);
             _NewMatrix.push_back(rowBuf);
             _MessageMatrix.push_back(rowBuf);
         }
-
         //filling both matrix with BGR values
         for (int i = 0; i < _y; i++) 
         {
             for (int j = 0; j < _x; j++)
             {
-
                 Vec3b Obuf = OriginalImage.at<Vec3b>(i, j);
                 _OriginalMatrix[i][j] = Obuf;
-               
-
                 _MessageMatrix[i][j] = Obuf;
-               /*
-
-                Vec3b Nbuf;
-                Nbuf.val[0]= NewImage.at<Vec3b>(i, j).val[0];
-                Nbuf.val[1] = NewImage.at<Vec3b>(i, j).val[1];
-                Nbuf.val[2] = NewImage.at<Vec3b>(i, j).val[2];
-                _NewMatrix[i][j].val[0] = Nbuf.val[0];
-                _NewMatrix[i][j].val[1] = Nbuf.val[1];
-                _NewMatrix[i][j].val[2] = Nbuf.val[2];
-                //_NewMatrix[i][j][1] = bufff[1];
-               // _NewMatrix[i][j][2] = bufff[2];*/
             }
         }
-        //showMatrixes();
 	}
-
-   
     //fields
 	vector <vector<Vec3b>> _OriginalMatrix,_NewMatrix,_MessageMatrix; //original matrix and new matrix storage
 	int _x, _y; //size of our images
